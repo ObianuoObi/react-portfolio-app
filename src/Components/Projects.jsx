0391befcfd5a73aee0ada31a3ade68d21d9aaa48
ObianuoObi <117560960+ObianuoObi@users.noreply.github.com>
2023-06-26 07:25:18 +0100

    Background url update

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -13,13 +13,13 @@ const Projects = () => {
           <p className='text-4xl font-bold inline border-b-4 text-blue-900 border-blue-900'>Projects</p>
         </div>
 
-        <div 
-        className='grid sm:grid-cols-2 md:grid-cols-3 gap-4 p-9'>
+        <div
+          className='grid sm:grid-cols-2 md:grid-cols-3 gap-4 p-9'>
 
-          <div style= {{backgroundImage: `url(${crypto})`}} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div '>
+          <div style={{ backgroundImage: `url(${crypto})` }} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div '>
 
             <div className='opacity-0 group-hover:opacity-100'>
-              
+
               <div className='pt-8 text-center'>
                 <a href="https://crypto-app-nine-tan.vercel.app/">
                   <button className=' text-center rounded-lg px-4 py-3 m-2 bg-blue-900 text-white font-bold text-lg '>Demo</button>
@@ -31,36 +31,36 @@ const Projects = () => {
 
             </div>
           </div>
-          <div style= {{backgroundImage: `url(${weather})`}} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+          <div style={{ backgroundImage: `url(${codex})` }} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
 
-<div className='opacity-0 group-hover:opacity-100'>
-  
-  <div className='pt-8 text-center'>
-    <a href="https://openai-codex-omega.vercel.app/">
-      <button className='text-center rounded-lg px-4 py-3 m-2 bg-blue-900 text-white font-bold text-lg '>Demo</button>
-    </a>
-    <a href="https://github.com/ObianuoObi/openai_codex">
-      <button className='text-center rounded-lg px-4 py-3 m-2 bg-blue-900 text-white font-bold text-lg '>Code</button>
-    </a>
-  </div>
+            <div className='opacity-0 group-hover:opacity-100'>
 
-</div>
-</div>
-<div style= {{backgroundImage: `url(${page})`}} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+              <div className='pt-8 text-center'>
+                <a href="https://openai-codex-omega.vercel.app/">
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-blue-900 text-white font-bold text-lg '>Demo</button>
+                </a>
+                <a href="https://github.com/ObianuoObi/openai_codex">
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-blue-900 text-white font-bold text-lg '>Code</button>
+                </a>
+              </div>
 
-<div className='opacity-0 group-hover:opacity-100'>
-  
-  <div className='pt-8 text-center'>
-    <a href="https://obianuoobi.github.io/page-template/">
-      <button className='text-center rounded-lg px-4 py-3 m-2 bg-blue-900 text-white font-bold text-lg '>Demo</button>
-    </a>
-    <a href="https://github.com/ObianuoObi/page-template">
-      <button className='text-center rounded-lg px-4 py-3 m-2 bg-blue-900 text-white font-bold text-lg '>Code</button>
-    </a>
-  </div>
+            </div>
+          </div>
+          <div style={{ backgroundImage: `url(${page})` }} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
 
-</div>
-</div>
+            <div className='opacity-0 group-hover:opacity-100'>
+
+              <div className='pt-8 text-center'>
+                <a href="https://obianuoobi.github.io/page-template/">
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-blue-900 text-white font-bold text-lg '>Demo</button>
+                </a>
+                <a href="https://github.com/ObianuoObi/page-template">
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-blue-900 text-white font-bold text-lg '>Code</button>
+                </a>
+              </div>
+
+            </div>
+          </div>
         </div>
       </div>
     </div>
